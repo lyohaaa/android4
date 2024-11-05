5f7211a3b6694f690d9eff309e0ec0388c76e328
cybersport <bzuev662@gmail.com>
2024-11-05 12:27:10 +0300

    xyz

--- app/src/main/java/com/example/kizhapkinzadanie6/MainActivity4.java
+++ app/src/main/java/com/example/kizhapkinzadanie6/MainActivity4.java
@@ -1,29 +1,52 @@
-package com.example.kizhapkinzadanie6;
+package com.example.kizhapkinzadanie7;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
+import android.provider.Settings;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-public class MainActivity4 extends AppCompatActivity {
+import com.example.kizhapkinzadanie7.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main4);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.Home) {
+                replaceFragment(new HomeFragment());
+            } else if (item.getItemId() == R.id.Profile) {
+                replaceFragment(new ProfileFragment());
+            } else if (item.getItemId() == R.id.Settings) {
+                replaceFragment(new SettingsFragment());
+            }
+            return true;
+        });
     }
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment).commit();
+    }
+}
 
     public void Navigate3(View v3) {
         Toast.makeText(MainActivity4.this, "Переход", Toast.LENGTH_SHORT).show();
